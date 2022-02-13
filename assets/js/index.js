78b8e57db71f61ff1d93a6218215c97b54f9ecd3
artur <unbrokenap@gmail.com>
2022-02-14 00:29:24 +0200

    add new files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,66 @@
-// Синтаксис Вычисляемого свойства - объект["поле"]
 
 
-// 1. Спросить у пользователя число и единицу измерения
-// В обьект tail в свойство с названием единицы изм. записать значение числа от пользователя
-
-
-// let unit = prompt("Введите название единици измерения");
-// let number = Number(prompt("Введите число"));
-
-// const tail = {
-//   [unit]: number
+// 1. создайте объект со структурой и заполните данными спросите у пользователя:
+// {
+// id,
+// firstname,
+// lastname,
+// age,
 // }
 
-// console.log(tail);
-
-
-
-// 2. Создать объект со свойствами box1, box2, box3, box... до 5
-// Заполнить свойства null
-// let content = prompt("Заполните содержимое коробок...");
 // const obj = {
-//   box1: null,
-//   box2: null,
-//   box3: null,
-//   box4: null,
-//   box5: null
-// }
-
+//   id: prompt("Введите свой id"),
+//   firstname: prompt("Введите свою имя"),
+//   lastname: prompt("Введите свою фамилию"),
+//   age: Number(prompt("Введите свой возраст"))
+// };
 // console.log(obj);
 
-// obj.box1 = [content];
-// obj.box2 = [content];
-// obj.box3 = [content];
-// obj.box4 = [content];
-// obj.box5 = [content];
+// 2. Создайте обьект-лифт
+// свойство хранящее текущий этаж, и метод смены этажа
+
+// const lift = {
+//   floor: 0,
+//   floorUp: function () {
+//     this.floor++;
+//     console.log(`Вы поднялись на ${lift.floor} этаж.`);
+//   },
+//   floorDown: function () {
+//     this.floor--;
+//     console.log(`Вы спустились на ${lift.floor} этаж.`);
+//   },
+// };
+
+// console.log(`Вы сейчас на ${lift.floor}-ом этаже.`);
+// lift.floorUp();
+// lift.floorUp();
+// lift.floorUp();
+// console.log(`Вы сейчас на ${lift.floor}-ом этаже.`);
+// lift.floorDown();
+// lift.floorDown();
+
+
+
+// 3. Клонировать обьект A и на его основе создать клон B
+// const objectA = {
+//   firstname: 'Ivan',
+//   lastname: 'Ivanov',
+//   age: 56
+// }
+
+// const clon = Object.assign({}, objectA);
+// clon.id = 33;
+
+// console.log(clon)
+
+
+// 4. Создать _шаблон_ обьекта Пользователь.
+// Принимать параметры - username, age, isMale, password.
+function User(username, age, isMale, password) {
+  this.username = username;
+  this.age = age;
+  this.isMale = isMale;
+  this.password = password;
+}
+
+console.log(new User('Ivan', '44', 'true', 'Ivan_44'));
